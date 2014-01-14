f12f0a0c162d715414829a1fdb8d2c7a050e7854
Aaron Coppa <aacoppa@gmail.com>
2014-01-14 12:20:41 -0500

    Worked on app.py, made different pages to be filled out

--- sites/qmail/app.py
+++ sites/qmail/app.py
@@ -1,14 +1,53 @@
-from flask import Flask
+from flask import Flask, session
 
-from .database import Database
+from game.database import Database
 
 app = Flask(__name__)
 app.secret_key = "X7jfId6Jb8T3sxVJ6xMQeEfGkqm3Qwft"
 db = Database("database.db")
 
-@route("/")
+@app.route("/")
 def index():
-    pass
+    return render_template("index.html")
+@app.route("/message/{mid}")
+def index(mid):
+    email = db.getMessage(mid)
+    return render_template("message.html", to=email['To'], from=email['From'], 
+                            sub=email['Subject'], msg=email['Message'])
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "GET":
+        return render_template("send.html", invalid=False)
+    else:
+        to = request.form['to']
+        if not db.valid_address(to):
+            return render_template("send.html", invalid=True)
+        
+        d.add_email(to, session['user'], request.form['subject'], request.form['message'], request.form['attaches'])
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", error="")
+    else:
+        user = request.form['user']
+        password = request.form['password']
+        if(db.valid(user, password)):
+            session['user'] = user
+        else:
+            return render_template("login.html", error="Invalid Password")
+
+@app.route("/create", methods=["GET", "POST"]) 
+def createAccount():
+    if request.method == "GET":
+        return render_template("create_account.html", error="")
+    else:
+        first, last, password, address = request.form['first'], request.form['last'], request.form['passwd'], request.form['addr']
+
+    error = db.register(first, last, password, address)
+    if not error:
+        session['user'] = address
+        return url_for("index")
+    else:
+        return render_template("create_account.html", error=error)
+
