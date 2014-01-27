253fe6d3074c12becbcfdfd142c231f4cd59929d
Ben Kurtovic <ben.kurtovic@gmail.com>
2014-01-27 18:49:16 -0500

    Schema update. Implement get_email().

--- game/database.py
+++ game/database.py
@@ -3,12 +3,19 @@ from logging import getLogger
 import sqlite3
 
 from . import utils
+from .attachment import Attachment
+from .email import Email
 from .user import User
 
 __all__ = ["Database"]
 
 SCHEMA_FILE = "schema.sql"
 SCHEMA_VERSION = 1
+
+EMAIL_SENDER = 0
+EMAIL_TO = 1
+EMAIL_CC = 2
+EMAIL_BCC = 3
 
 class Database(object):
     """Represents an SQLite database storing all game information."""
@@ -50,14 +57,43 @@ class Database(object):
     # Generic object builders
 
     def get_user(self, user_id):
-        """Return the User object associated with the given user ID."""
+        """Return the User object associated with the given user ID.
+
+        Raises IndexError if the user does not exist.
+        """
         query = "SELECT * FROM qmail_users WHERE qmu_id = ?"
         result = self._execute(query, user_id)
         return User(*result[0])
 
     def get_email(self, email_id, user_id):
-        """Get an email from an ID if the user has permission to view it."""
-        if
+        """Get an email from an ID if the user has permission to view it.
+
+        Returns None if no email could be found or the user isn't allowed to
+        view it. Raises IndexError if the user does not exist.
+        """
+        user = self.get_user(user_id)
+        query = """SELECT qmm_type, qmm_address, qmm_name
+                   FROM qmail_email_members WHERE qmm_email = ?"""
+        members = self._execute(query, email_id)
+        if not [mem for mem in members if mem[1] == user.address]:
+            return None  # User not permitted to view
+        try:
+            sender = [(mem[1], mem[2]) for mem in members if mem[0] == EMAIL_SENDER][0]
+        except IndexError:
+            return None  # No senders -> email doesn't exist
+        to = [(mem[1], mem[2]) for mem in members if mem[0] == EMAIL_TO]
+        cc = [(mem[1], mem[2]) for mem in members if mem[0] == EMAIL_CC]
+        bcc = [(mem[1], mem[2]) for mem in members if mem[0] == EMAIL_BCC]
+
+        query = "SELECT * FROM qmail_emails WHERE qme_id = ?"
+        _, subject, body = self._execute(query, email_id)[0]
+
+        attachments = []
+        query = "SELECT * FROM qmail_attachments WHERE qma_email = ?"
+        for aid, _, filename, content in self._execute(query, email_id):
+            attachments.append(Attachment(aid, filename, content))
+
+        return Email(email_id, sender, subject, body, to, cc, bcc, attachments)
 
     # Qmail
 
--- schema.sql
+++ schema.sql
@@ -25,34 +25,26 @@ CREATE TABLE qmail_users (
     qmu_password_salt BLOB
 );
 
-DROP TABLE IF EXISTS qmail_mailboxes;
-CREATE TABLE qmail_mailboxes (
-    qmm_id INTEGER PRIMARY KEY,
-    qmm_user INTEGER
-);
-
 DROP TABLE IF EXISTS qmail_emails;
 CREATE TABLE qmail_emails (
     qme_id INTEGER PRIMARY KEY,
-    qme_sender INTEGER,
     qme_subject TEXT,
-    qme_body TEXT,
-    qme_has_attachments BOOLEAN
+    qme_body TEXT
 );
 
-DROP TABLE IF EXISTS qmail_recipients;
-CREATE TABLE qmail_recipients (
-    qmr_id INTEGER PRIMARY KEY,
-    qmr_message INTEGER,
-    qmr_type INTEGER,
-    qmr_address BLOB,
-    qmr_name TEXT
+DROP TABLE IF EXISTS qmail_email_members;
+CREATE TABLE qmail_email_members (
+    qmm_id INTEGER PRIMARY KEY,
+    qmm_email INTEGER,
+    qmm_type INTEGER,
+    qmm_address BLOB,
+    qmm_name TEXT
 );
 
 DROP TABLE IF EXISTS qmail_attachments;
 CREATE TABLE qmail_attachments (
     qma_id INTEGER PRIMARY KEY,
-    qma_message INTEGER,
+    qma_email INTEGER,
     qma_filename BLOB,
     qma_content BLOB
 );
